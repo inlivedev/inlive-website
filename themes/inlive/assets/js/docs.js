189d01ae72205984a17bc02d44b22eee784b150e
Faiq Naufal <faiq.naufal@asumsi.co>
2022-03-04 08:00:32 +0700

    feat: add javascript assets
    add specific javascript for docs pages and global pages

---
+++ themes/inlive/assets/js/docs.js
@@ -0,0 +1,15 @@
+const sideNavOverlay = document.querySelector('#side-nav-overlay');
+const sideNavMenu = document.querySelector('#side-nav-menu');
+
+const handleToggleSideNavMenu = () => {
+  if (sideNavOverlay) sideNavOverlay.classList.toggle('hidden');
+  if (sideNavMenu) sideNavMenu.classList.toggle('show');
+}
+
+const handleBackToTop = () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' })
+}
+
+const handleToggleSearchBar = () => {
+
+}
