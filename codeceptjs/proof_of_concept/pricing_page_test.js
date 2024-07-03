1468d5f2efa8b3160b2cabeb43d62235bce65c19
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2024-07-03 15:15:09 +0700

    refactor: remove unused modules and files (#239)
    - Remove unused dependencies including codeceptjs
    - Remove unused deployment files (cloudbuild, k8s, dockerfile). We are using cloudflare pages for testing and github pages for production
    - Remove interactive widget landing page
    - Remove blog post related to widget

--- codeceptjs/proof_of_concept/pricing_page_test.js
+++
@@ -1,9 +0,0 @@
-Feature('pricing page');
-
-Scenario("click Pricing", ({ I }) => {
-    I.amOnPage("/");
-    I.click("Pricing");
-    I.seeInCurrentUrl("/pricing");
-    I.see("Choose Your Plan");
-    I.see("Frequently Asked Questions");
-});
